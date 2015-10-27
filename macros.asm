d2cebc472598b61d51663ac0bdb3bb38248231be
espes <espes@pequalsnp.com>
2015-10-27 20:33:55 +1100

    use loadZ in clear16

--- macros.asm
+++ macros.asm
@@ -1,17 +1,15 @@
+.macro loadZ
+    ldi ZH, HIGH(@0)
+    ldi ZL, LOW(@0)
+.endmacro
 
 ; The macro clear16s a word (2 bytes) in a memory
 ; the parameter @0 is the memory address for that word
 .macro clear16
-    ldi YL, low(@0) ; load the memory address to Y
-    ldi YH, high(@0)
+    loadZ @0
     clr temp
     st Y+, temp ; clear16 the two bytes at @0 in SRAM
     st Y, temp
-.endmacro
-
-.macro loadZ
-    ldi ZH, HIGH(@0)
-    ldi ZL, LOW(@0)
 .endmacro
 
 #define CODE(x) ((x) << 1)
