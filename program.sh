db85d2d72c1c8b2051b8a86f6fad78c2d7070cd0
George Caley <george@caley.com.au>
2015-10-27 15:36:51 +1100

    added avrdude + programming script

---
+++ program.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+AVRDUDE_EXE=./avrdude
+AVRDUDE_CONF=avrdude.conf
+PORT="`echo /dev/tty.usbmodem*`"
+HEX=$1
+
+if ! [ -f "$AVRDUDE_EXE" ]; then
+    echo "avrdude executable $AVRDUDE_EXE not found"
+    exit 1
+elif ! [ -f "$AVRDUDE_CONF" ]; then
+    echo "avrdude conf file $AVRDUDE_CONF not found"
+    exit 1
+elif ! [ -e "$PORT" ]; then
+    echo "Port $PORT not found"
+    exit 1
+elif ! [ -f "$HEX" ]; then
+    echo "File $HEX not found"
+    exit 1
+fi
+
+"$AVRDUDE_EXE" -C "$AVRDUDE_CONF" -c wiring -p m2560 -P "$PORT" -b 115200 -U "flash:w:$HEX:i" -D
